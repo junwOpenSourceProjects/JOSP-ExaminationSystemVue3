7df98abce92985a2f5948c942cfa1b8108fd816b
junw <wo1261931780@gmail.com>
2026-06-01 20:17:16 +0800

    chore: remove legacy Vue2 src.backup directory

--- src.backup/views/demo/multi-level/children/level2.vue
+++
@@ -1,8 +0,0 @@
-<!-- 多级菜单第2级 -->
-<template>
-  <div style="padding: 30px">
-    <el-alert :closable="false" title="菜单二级" type="success">
-      <router-view />
-    </el-alert>
-  </div>
-</template>
